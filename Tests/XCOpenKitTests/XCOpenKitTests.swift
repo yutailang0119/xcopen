86afdb766268f85b80d17d71a6924755ed215750
yutailang0119 <muta.yutaro@gmail.com>
2018-03-30 01:13:24 +0900

    Fix to rest tests

--- Tests/XCOpenKitTests/XCOpenKitTests.swift
+++ Tests/XCOpenKitTests/XCOpenKitTests.swift
@@ -2,15 +2,4 @@ import XCTest
 @testable import XCOpenKit
 
 class XCOpenKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(XCOpenKit().text, "Hello, World!")
-    }
-
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
