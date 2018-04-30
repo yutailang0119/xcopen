5592513a3ea57b78b2e72b1b1544a5b0f6b07464
yutailang0119 <muta.yutaro@gmail.com>
2018-04-30 23:22:30 +0900

    Add Question and can select from multiple corresponding files

--- Sources/XCOpenKit/OpenTool.swift
+++ Sources/XCOpenKit/OpenTool.swift
@@ -17,10 +17,15 @@ struct OpenTool {
         let all = try path.recursiveChildren()
         let packages = all.flatMap { $0.glob(fileName) }
 
-        if packages.isEmpty {
+        switch packages.count {
+        case 0:
             throw XCOpenError.failedRun(description: "Nomatch fileName: \(fileName)")
+        case 1:
+            open(of: packages[0])
+        default:
+            let selectedPackage = try selectPackage(from: packages)
+            open(of: selectedPackage)
         }
-        open(of: packages[0])
     }
 
     private func open(of path: Path) {
@@ -29,6 +34,25 @@ struct OpenTool {
         process.arguments = ["-c", "open \(path.description)"]
         process.launch()
         process.waitUntilExit()
+    }
+
+    private func selectPackage(from packages: [Path]) throws -> Path {
+        typealias Inquiry = (sentence: String, choices: [String])
+
+        let inquiry: Inquiry = packages.enumerated()
+            .reduce(into: Inquiry(sentence: "Which do you want to open? Please select index\n", choices: [])) { result, value in
+                result.sentence += "[\(value.offset)]: \(value.element.description)\n"
+                result.choices.append(String(value.offset))
+        }
+
+        let question = Question()
+        let selected = question.ask(inquiry.sentence, answers: inquiry.choices)
+
+        guard let index = Int(selected) else {
+            throw XCOpenError.failedRun(description: "")
+        }
+
+        return packages[index]
     }
 
 }
---
+++ Sources/XCOpenKit/Question.swift
@@ -0,0 +1,35 @@
+//
+//  Question.swift
+//  XCOpenKit
+//
+//  Created by Yutaro Muta on 2018/04/30.
+//
+
+import Foundation
+
+struct Question {
+
+    let printer: (String) -> Void
+    init(printer: @escaping (String) -> Void = { print($0) }) {
+        self.printer = printer
+    }
+
+    func ask(_ question: String, answers: [String]) -> String {
+        printer(question)
+
+        func ask() -> String {
+            guard let answer = readLine() else { return "" }
+            let lowercasedAnswers = answers.map { $0.lowercased() }
+            if !lowercasedAnswers.contains(answer.lowercased()) {
+                print("You must respond with one of the following:\n\(answers.joined(separator: "\n"))")
+                return ask()
+            }
+            return answer
+        }
+        return ask()
+    }
+
+    func confirmation(_ question: String) -> Bool {
+        return ask("\(question) (y/n)", answers: ["y", "n"]) == "y"
+    }
+}
