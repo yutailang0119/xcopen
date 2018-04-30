8271a95a9e7ad5521256d27e38270e6026eaba83
yutailang0119 <muta.yutaro@gmail.com>
2018-05-01 01:21:45 +0900

    Add RxSwift 4.1.2

--- Package.swift
+++ Package.swift
@@ -11,11 +11,12 @@ let package = Package(
     ], 
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.1.2"),	
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0"),
     ],
     targets: [
         .target(name: "xcopen", dependencies: ["XCOpenKit"]),
-        .target(name: "XCOpenKit", dependencies: ["PathKit", "Utility"]),
+        .target(name: "XCOpenKit", dependencies: ["PathKit", "RxSwift", "Utility"]),
         .testTarget(name: "XCOpenKitTests", dependencies: ["XCOpenKit"])
     ]
 )
