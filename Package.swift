51a55e17d473a4cb27d6487aeabd58b059c1dd74
yutailang0119 <muta.yutaro@gmail.com>
2018-09-02 17:36:45 +0900

    Introduce ProgressSpinnerKit

--- Package.swift
+++ Package.swift
@@ -13,10 +13,11 @@ let package = Package(
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.1.2"),	
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0"),
+        .package(url: "https://github.com/yutailang0119/ProgressSpinnerKit", from: "0.1.0"),
     ],
     targets: [
         .target(name: "xcopen", dependencies: ["XCOpenKit"]),
-        .target(name: "XCOpenKit", dependencies: ["PathKit", "RxSwift", "Utility"]),
+        .target(name: "XCOpenKit", dependencies: ["PathKit", "RxSwift", "Utility", "ProgressSpinnerKit"]),
         .testTarget(name: "XCOpenKitTests", dependencies: ["XCOpenKit"])
     ]
 )
