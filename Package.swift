67245b35059515956efcc9ca7a01a43e1cb261cc
yutailang0119 <muta.yutaro@gmail.com>
2019-10-07 23:44:35 +0900

    Update ProgressSpinnerKit from 0.3.0 to 0.5.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.4.0"),
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.5.0"),
-        .package(url: "https://github.com/yutailang0119/ProgressSpinnerKit", from: "0.1.0"),
+        .package(url: "https://github.com/yutailang0119/ProgressSpinnerKit", from: "0.3.0"),
     ],
     targets: [
         .target(name: "xcopen", dependencies: ["XCOpenKit"]),
