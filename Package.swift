b038ed45a1cba1a82da922e5f95cc11597cab062
yutailang0119 <muta.yutaro@gmail.com>
2019-10-08 20:06:46 +0900

    Update RXSwift from 4.4.0 to 5.0.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ], 
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.4.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.5.0"),
         .package(url: "https://github.com/yutailang0119/ProgressSpinnerKit", from: "0.3.0"),
     ],
